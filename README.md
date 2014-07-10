4bc54d2ee5be393a0a9ce0cdf96f1bd54381ff7c
Naoki Nishida <domitry@gmail.com>
2014-07-10 17:46:40 +0900

    Initial commit from local repository

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ cytoscape-ipy
 =============
 
 Cytoscape.js on IPython notebook
+
+This is only a demo. This repository will not be updated no longer.
+
+Demo code from [cytoscape.github.io](http://jsbin.com/qohop/3/edit)
---
+++ cytoscape-ipy/main.py
@@ -0,0 +1,40 @@
+import uuid
+import json
+import os
+
+def init_ipynb():
+    from IPython.core.display import display, HTML
+
+    path = os.path.abspath(os.path.dirname(__file__)) + '/init_ipy.js'
+    html = open(path).read()
+    return display(HTML(html))
+
+def plot(nodes=None, edges=None):
+    from jinja2 import Template
+    from IPython.core.display import display, HTML
+
+    if nodes==None:
+        nodes = [
+            { 'data': { 'id': 'a' } },
+            { 'data': { 'id': 'b' } },
+            { 'data': { 'id': 'c' } },
+            { 'data': { 'id': 'd' } },
+            { 'data': { 'id': 'e' } }
+        ]
+
+    if edges==None:
+        edges = [
+            { 'data': { 'id': 'a', 'weight': 1, 'source': 'a', 'target': 'e' } },
+            { 'data': { 'id': 'ab', 'weight': 3, 'source': 'a', 'target': 'b' } },
+            { 'data': { 'id': 'be', 'weight': 4, 'source': 'b', 'target': 'e' } },
+            { 'data': { 'id': 'bc', 'weight': 5, 'source': 'b', 'target': 'c' } },
+            { 'data': { 'id': 'ce', 'weight': 6, 'source': 'c', 'target': 'e' } },
+            { 'data': { 'id': 'cd', 'weight': 2, 'source': 'c', 'target': 'd' } },
+            { 'data': { 'id': 'de', 'weight': 7, 'source': 'd', 'target': 'e' } }
+        ]
+
+    path = os.path.abspath(os.path.dirname(__file__)) + '/template.html'
+    template = Template(open(path).read())
+    html = template.render(nodes=json.dumps(nodes), edges = json.dumps(edges))
+    return display(HTML(html))
+
